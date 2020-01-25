2f961cde22d817f89d589054cee34abffa84b5cc
snoopy <snoopy@ayumi-no-MacBook-Air.local>
2020-01-25 20:58:09 +0900

    myfrtstcommit

---
+++ part1_8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import statistics
+
+
+list = [1,3,100]
+statistics.mean(list)
+
+
+# In[5]:
+
+
+def cubed(int):
+    return int**3
+
+
+# In[ ]:
+
+
+
+
